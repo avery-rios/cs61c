9ecd6252d2631bcc368633f45dc614fa1ff41c62
Avery Rios <avery-dd70@outlook.com>
2024-02-17 22:25:52 +0800

    [lab] finish 01-Number Rep, C and CGDB

--- labs/lab01/ll_cycle.c
+++ labs/lab01/ll_cycle.c
@@ -1,7 +1,18 @@
-#include <stddef.h>
 #include "ll_cycle.h"
+#include <stddef.h>
 
 int ll_has_cycle(node *head) {
-    /* your code here */
-    return 0;
+  node *tort = head, *hare = head;
+  while (hare) {
+    hare = hare->next;
+    if (!hare) {
+      return 0;
+    }
+    hare = hare->next;
+    tort = tort->next;
+    if (hare == tort) {
+      return 1;
+    }
+  }
+  return 0;
 }
