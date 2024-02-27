6ce81c35c98a77bdaea6b4bd37ba8b15ee9e5604
Avery Rios <avery-dd70@outlook.com>
2024-02-27 18:40:45 +0800

    [lab] finish 02-Advanced C

--- labs/lab02/lfsr.c
+++ labs/lab02/lfsr.c
@@ -1,10 +1,14 @@
-#include <stdio.h>
+#include "lfsr.h"
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+  const uint16_t rv = *reg;
+  uint16_t b = rv;
+  b ^= rv >> 2;
+  b ^= rv >> 3;
+  b ^= rv >> 5;
+  *reg = (rv >> 1) | ((b & 1) << 15);
 }
-
