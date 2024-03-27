b4e4fcac4cc9404a494b6df27efad8512d3c2a6d
Avery Rios <avery-dd70@outlook.com>
2024-03-27 18:03:50 +0800

    split devShell

--- flake.nix
+++ flake.nix
@@ -8,9 +8,12 @@
 
   outputs = { nixpkgs, ... }: {
     devShells = {
-      "x86_64-linux".default = let pkgs = nixpkgs.legacyPackages."x86_64-linux";
-      in pkgs.mkShell {
-        buildInputs = with pkgs; [ gcc gdb valgrind gnumake openjdk ];
+      "x86_64-linux" = let pkgs = nixpkgs.legacyPackages."x86_64-linux";
+      in {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [ gcc gdb valgrind gnumake openjdk ];
+        };
+        evoluvion = pkgs.mkShell { buildInputs = [ pkgs.openjdk ]; };
       };
     };
   };
